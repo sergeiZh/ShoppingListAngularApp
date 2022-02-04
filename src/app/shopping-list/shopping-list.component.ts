6e0fbf454f50cf25f0233ed20581b78bc75d26ab
Siarhei Zhabrykau <siarhei_zhabrykau@epam.com>
2022-02-04 12:48:28 +0100

    Build base components and relations

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingridient } from '../shared/ingridient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingridient[] = [
+    new Ingridient('apple', 7),
+    new Ingridient('lemon', 2)
+  ];
 
   constructor() { }
 
